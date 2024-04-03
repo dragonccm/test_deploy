636498a607b6d6dd3df8ac45959fb6d6be988d48
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-04-03 16:00:45 +0700

    .

--- components/forms/LikeThread.tsx
+++ components/forms/LikeThread.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import Image from "next/image";
-
-import { addLikeToThread } from "@/lib/actions/thread.actions";
 import { useEffect, useState } from "react";
+import { addLikeToThread } from "@/lib/actions/thread.actions";
 
 interface Props {
   threadId: string;
@@ -18,71 +17,68 @@ function LikeThread({
   userId,
   isLike,
 }: Props) {
-  const [isLiked, setIsLiked] = useState(false); 
-  const [currlike, setTotalLike] = useState(0); 
+  const [isLiked, setIsLiked] = useState(false);
+  const [currlike, setTotalLike] = useState(totalLike.length);
+  const [isLiking, setIsLiking] = useState(false);
+  const [likeTimeout, setLikeTimeout] = useState<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     if (isLike !== undefined) {
-      setIsLiked(isLike);
-    }
-    setTotalLike(totalLike.length)
-
-  }, []);
-
-  useEffect(() => {
-    if (isLike !== undefined) {
-      setIsLiked(isLike);
+      setIsLiked(!isLike);
     }
   }, [isLike]);
 
-  const handleLikeClick = async () => {
+  const handleLikeClick = () => {
     setIsLiked((prevIsLiked) => {
       const updatedIsLiked = !prevIsLiked;
 
-      addLikeToThread(JSON.parse(threadId), userId, updatedIsLiked)
-        .then(() => {
-          setIsLiked(updatedIsLiked);
-          if(updatedIsLiked){
-            setTotalLike(currlike-1)
-          }else{
-            setTotalLike(currlike+1)
-          }
-        })
-        .catch((error) => {
-          console.error("Error updating like:", error);
-        });
+      setIsLiking(true);
+      setTotalLike((prevTotalLike) => prevTotalLike + (updatedIsLiked ? 1 : -1));
+
+      
+      if (likeTimeout) {
+        clearTimeout(likeTimeout);
+      }
+
+      const timeout = setTimeout(() => {
+        addLikeToThread(JSON.parse(threadId), userId, updatedIsLiked)
+          .then(() => {
+            
+          })
+          .catch((error) => {
+            console.error("Error updating like:", error);
+          })
+          .finally(() => {
+            setIsLiking(false);
+          });
+      }, 2000); 
+
+      setLikeTimeout(timeout);
 
       return updatedIsLiked;
     });
+
+    setIsLiking(false); 
   };
+
   return (
     <>
-      {isLiked === false ? (<div className="item-center flex gap-2">
+      <div className="item-center flex gap-2">
         <Image
-          src='/assets/heart-gray-filled.svg'
-          alt='heart'
+          src={isLiked ? "/assets/heart-gray-filled.svg" :  "/assets/heart-gray.svg" }
+          alt="heart"
           width={24}
           height={24}
-          className='cursor-pointer object-contain'
+          className="cursor-pointer object-contain"
           onClick={handleLikeClick}
         />
         <p className="mt-1 text-subtle-medium text-gray-1">{currlike} like</p>
-      </div>)
-        :
-        (<div className="item-center flex gap-2">
-          <Image
-            src='/assets/heart-gray.svg'
-            alt='heart'
-            width={24}
-            height={24}
-            className='cursor-pointer object-contain'
-            onClick={handleLikeClick}
-          />
-          <p className="mt-1 text-subtle-medium text-gray-1">{currlike} like</p>
-        </div>)}
+      </div>
 
+      {isLiking}
     </>
   );
 }
+
 
 export default LikeThread;
